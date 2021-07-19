a519d101d1b08ddaab00ce4211a09539240da38d
isabotasso <isabellabotasso20@gmail.com>
2021-07-19 13:57:24 -0300

    11-exercicio.rb

---
+++ 11-exercicio.rb
@@ -0,0 +1,19 @@
+# Exercicio 11
+# 1) Fazer uma função chamada 'posicao_no_array' que recebe um array e um valor qualquer como parâmetros
+#  e que deve retornar a posição da primeira ocorrência do valor no array.
+#  Se o valor não existir no array, retornar -1.
+
+def posicao_no_array (array, valor)
+    for n in (0..array.size-1)
+        if array[n] == valor
+            return n
+        end
+    end
+    return "-1"
+end
+
+array = [10, 20, 30, 40, 50, 60]
+puts posicao_no_array(array, 55)
+puts posicao_no_array(array, 60)
+puts posicao_no_array(array, 10)
+puts posicao_no_array(array, 44)
